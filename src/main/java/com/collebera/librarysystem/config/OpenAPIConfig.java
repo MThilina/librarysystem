9120736c754f18952dde15b49996d041191a54e5
thilinam <thilinamanawadu@gmail.com>
2024-05-20 22:57:03 +0530

    open api implementation (api documentation)

---
+++ src/main/java/com/collebera/librarysystem/config/OpenAPIConfig.java
@@ -0,0 +1,19 @@
+package com.collebera.librarysystem.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info().title("Library System API")
+                        .description("API documentation for the Library System")
+                        .version("1.0"));
+    }
+}
