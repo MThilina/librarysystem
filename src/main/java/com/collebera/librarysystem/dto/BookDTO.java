850bf85611b273a538759f13769a86efaeb3ef1b
thilinam <thilinamanawadu@gmail.com>
2024-05-21 10:13:39 +0530

    adding validations

--- src/main/java/com/collebera/librarysystem/dto/BookDTO.java
+++ src/main/java/com/collebera/librarysystem/dto/BookDTO.java
@@ -1,11 +1,19 @@
 package com.collebera.librarysystem.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class BookDTO {
     private Long id;
+    @NotBlank @NotEmpty @NotNull
     private String isbn;
+    @NotBlank @NotEmpty @NotNull
     private String title;
+    @NotBlank @NotEmpty @NotNull
+    @Email
     private String author;
 }
